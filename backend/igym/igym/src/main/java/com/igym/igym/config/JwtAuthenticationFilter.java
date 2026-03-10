7c97d7f593e6b8558f4395448e8706e65d08eff0
kamyllevictoria <sharmankamylle@gmail.com>
2026-03-10 16:01:48 -0300

    feat: adding methods at jwtService

--- backend/igym/igym/src/main/java/com/igym/igym/config/JwtAuthenticationFilter.java
+++ backend/igym/igym/src/main/java/com/igym/igym/config/JwtAuthenticationFilter.java
@@ -11,7 +11,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
